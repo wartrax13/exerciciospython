e21219967a46cd605b57e6905c53d5a8d63a26d0
Pedro <pedro.moisesandrade@gmail.com>
2021-07-10 17:07:55 -0300

    Criando um Banco

--- pythonorientacao/oo/conta.py
+++ pythonorientacao/oo/conta.py
@@ -1,19 +1,37 @@
+import datetime
+
+
+class Historico:
+    def __init__(self):
+        self.data_abertura = datetime.datetime.today()
+        self.transacoes = []
+
+    def imprime(self):
+        print('Data abertura: {}'.format(self.data_abertura))
+        print('Transações: ')
+        for t in self.transacoes:
+            print('-', t)
+
+
 class Cliente:
     def __init__(self, nome, sobrenome, cpf):
         self.nome = nome
         self.sobrenome = sobrenome
         self.cpf = cpf
 
+
 class Conta:
-    def __init__(self, numero, cliente, saldo, limite = 1000.0):
+    def __init__(self, numero, cliente, saldo, limite=1000.0):
         print('inicializando uma conta')
         self.numero = numero
-        self.titular = Cliente
+        self.cliente = cliente
         self.saldo = saldo
         self.limite = limite
+        self.historico = Historico()
 
     def deposita(self, valor):
         self.saldo += valor
+        self.historico.transacoes.append('Depósito de {}'.format(valor))
 
     def saca(self, valor):
         if self.saldo < valor:
@@ -22,10 +40,12 @@ class Conta:
 
         else:
             self.saldo -= valor
+            self.historico.transacoes.append('Saque de {}'.format(valor))
             return True
 
     def extrato(self):
         print("numero: {} \nsaldo: {}".format(self.numero, self.saldo))
+        self.historico.transacoes.append('Tirou extrato - saldo de {}'.format(self.saldo))
 
     def transfere_para(self, destino, valor):
         retirou = self.saca(valor)
@@ -33,4 +53,5 @@ class Conta:
             return False
         else:
             destino.deposita(valor)
+            self.historico.transacoes.append('Transferencia de {} para conta {}'.format(valor, destino.numero))
         return True
--- pythonorientacao/oo/outroarquivo.py
+++ pythonorientacao/oo/outroarquivo.py
@@ -1,14 +1,14 @@
 from conta import Conta, Cliente
 
-conta = Conta('123-4', 'João', 120.0, 1000.0)
-conta2 = Conta('123-5', 'Pedro', 100.0, 1000.0)
+cliente1 = Cliente('João', 'Oliveira', '11111111111-11')
+cliente2 = Cliente('José', 'Azevedo', '222222222-22')
+conta1 = Conta('123-4', cliente1, 1000.0)
+conta2 = Conta('123-5', cliente2, 1000.0)
+conta1.deposita(100.0)
+conta1.saca(50.0)
+conta1.transfere_para(conta2, 200.0)
+conta1.extrato()
+conta1.historico.imprime()
 
-cliente = Cliente('João', 'Oliveira', '1111111111-1')
-minha_conta = Conta('123-4', cliente, 120.0, 1000.0)
-#conta.deposita(20.0)
-#conta.extrato()
-#conta.saca(500)
-#conta.extrato()
+conta2.historico.imprime()
 
-#print(minha_conta.titular.nome)
-print(type(conta.titular))

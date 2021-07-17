454564eeed29b00407596090d8e4be20c2cd43ce
Pedro <pedro.moisesandrade@gmail.com>
2021-07-17 15:23:44 -0300

    Tkinder com banco de dados

--- programas/primeiro.py
+++ programas/primeiro.py
@@ -1,14 +1,15 @@
 from tkinter import *
 
+
 class Application:
     def __init__(self, master=None):
-        self.fontePadrao = ("Arial", "10")
+        self.fontePadrao = ('Arial', '10')
         self.primeiroContainer = Frame(master)
-        self.primeiroContainer["pady"] = 10
+        self.primeiroContainer['pady'] = 10
         self.primeiroContainer.pack()
 
         self.segundoContainer = Frame(master)
-        self.segundoContainer["padx"] = 20
+        self.segundoContainer['pady'] = 20
         self.segundoContainer.pack()
 
         self.terceiroContainer = Frame(master)
@@ -19,11 +20,11 @@ class Application:
         self.quartoContainer["pady"] = 20
         self.quartoContainer.pack()
 
-        self.titulo = Label(self.primeiroContainer, text="Dados do usuário")
-        self.titulo["font"] = ("Arial", "10", "bold")
+        self.titulo = Label(self.primeiroContainer, text='Digite algo: ')
+        self.titulo['font'] = ('Arial', '10', 'bold')
         self.titulo.pack()
 
-        self.nomeLabel = Label(self.segundoContainer,text="Nome", font=self.fontePadrao)
+        self.nomeLabel = Label(self.segundoContainer, text="Impressão de texto", font=self.fontePadrao)
         self.nomeLabel.pack(side=LEFT)
 
         self.nome = Entry(self.segundoContainer)
@@ -31,33 +32,21 @@ class Application:
         self.nome["font"] = self.fontePadrao
         self.nome.pack(side=LEFT)
 
-        self.senhaLabel = Label(self.terceiroContainer, text="Senha", font=self.fontePadrao)
-        self.senhaLabel.pack(side=LEFT)
-
-        self.senha = Entry(self.terceiroContainer)
-        self.senha["width"] = 30
-        self.senha["font"] = self.fontePadrao
-        self.senha["show"] = "*"
-        self.senha.pack(side=LEFT)
 
         self.autenticar = Button(self.quartoContainer)
-        self.autenticar["text"] = "Autenticar"
+        self.autenticar["text"] = "Imprimir"
         self.autenticar["font"] = ("Calibri", "8")
         self.autenticar["width"] = 12
-        self.autenticar["command"] = self.verificaSenha
+        self.autenticar["command"] = self.verificarSenha
         self.autenticar.pack()
 
         self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
         self.mensagem.pack()
 
     #Método verificar senha
-    def verificaSenha(self):
-        usuario = self.nome.get()
-        senha = self.senha.get()
-        if usuario == "usuariodevmedia" and senha == "dev":
-            self.mensagem["text"] = "Autenticado"
-        else:
-            self.mensagem["text"] = "Erro na autenticação"
+    def verificarSenha(self):
+        texto = self.nome.get()
+        self.mensagem['text'] = texto
 
 
 root = Tk()

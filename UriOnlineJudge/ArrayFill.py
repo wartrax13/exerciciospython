7cde32d16ef86ffb4a1c4b60bd7b69aa9195153d
Pedro <pedro.moisesandrade@gmail.com>
2021-07-25 21:29:09 -0300

    Soluções aritmeticas

---
+++ UriOnlineJudge/ArrayFill.py
@@ -0,0 +1,5 @@
+x = int(input())
+for i in range(0, 11):
+    print('N[{}] = {}'.format(i, x))
+    i += 1
+    x = x * 2
--- crash_course_google/Semana 3 - Recursion.py
+++ crash_course_google/Semana 3 - Recursion.py
@@ -5,6 +5,28 @@
 
 print(factorial(1000))
 '''
-host_addresses = {"router": "192.168.1.1", "localhost": "127.0.0.1", "google": "8.8.8.8"}
+'''host_addresses = {"router": "192.168.1.1", "localhost": "127.0.0.1", "google": "8.8.8.8"}
 print(host_addresses.keys())
 
+'''
+
+x = 51
+s = 0
+for i in range (0,10):
+  print(f'N[{i}] = {x}')
+  i += 1
+  x = x * 2
+
+  '''
+N[1] = 1
+N[2] = 2
+N[3] = 4
+N[4] = 8
+N[5] = 16
+N[6] = 32
+N[7] = 64
+N[8] = 128
+N[9] = 256
+N[10] = 512
+
+  '''
